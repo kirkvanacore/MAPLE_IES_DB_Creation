808eccf8ac36da874fd8344dc9b59bee50db3b97
kirkvanacore <55771791+kirkvanacore@users.noreply.github.com>
2022-06-15 10:03:25 -0400

    added assistments_user_ids

--- 01_table_creation_scripts/crosswalk_script.R
+++ 01_table_creation_scripts/crosswalk_script.R
@@ -33,11 +33,11 @@ options(scipen = 100)
 ies_research_con <- dbConnect(RSQLite::SQLite(), "ies_research schema/maple_ies_research.db")
 
 ### Load 
-cross <- read.csv("Crosswalk_CSVs_20220201/Students-Table 1.csv", na.strings = c("", " "))
+cross <- read.csv("02_data_source_files/Crosswalk_CSVs_20220201/Students-Table 1.csv", na.strings = c("", " "))
 colnames(cross)
 
 ### add condition using the assess file 
-assess <- read.csv("DATA20220202_4092 copy.csv")
+assess <- read.csv("02_data_source_files/DATA20220202_4092.csv")
 colnames(assess)
 
 cross <- cross %>%
@@ -58,7 +58,7 @@ cross <- cross %>%
 
 
 #### ADDING FH2T User_id
-FH2T<-read.csv("OLD_FH2T_aggregation_overall_merge_2021_03_25.csv") %>%
+FH2T<-read.csv("04_data_archive/OLD_FH2T_aggregation_overall_merge_2021_03_25.csv") %>%
   select(
     student_number,
     student_id,
@@ -66,7 +66,6 @@ FH2T<-read.csv("OLD_FH2T_aggregation_overall_merge_2021_03_25.csv") %>%
     o_num_visit
   )
 colnames(FH2T)
-
 
 cross <- cross %>%
   left_join(FH2T %>%
@@ -85,7 +84,21 @@ table(is.na(FH2T$userID))
 # there are 501 students who were assigned to the FH2T Condition, who did not have any activity in FH2T
 
 ### ADDING ASSISTments 
-assist<-read.csv("ASSISTment_student_id_crosswalk.csv")
+# need to get the student ids from this data
+old_logs <- read.csv("04_data_archive/assistments_ies_dataset_2021_ 0322_fromAnthony.csv")
+colnames(old_logs)
+length(unique(old_logs$student_id))
+length(unique(old_logs$assistments_user_id))
+
+
+cross<-cross %>%
+  left_join(old_logs %>%
+              select(student_id,
+                     assistments_user_id) %>%
+            distinct(),
+            by = c("student_id"))
+
+table(is.na(cross$assistments_user_id))
 
 # final cleaning before save
 
@@ -93,9 +106,11 @@ cross <- cross %>%
   select(StuID,
          STUDID,
          student_id,
+         assistments_user_id,
          userID,
-         condition_assignment) %>%
-  rename(fh2t_user_id = userID)
+         condition_assignment,
+         rdm_condition) %>%
+  dplyr::rename(fh2t_user_id = userID)
   
 
 ### Save Crosswalk as csv
@@ -103,5 +118,5 @@ write.csv(cross, "ies_research schema/student_id_crosswalk.csv")
 
 ### Save crosswalk in maple_ies_research db
 
-RSQLite::dbWriteTable(ies_research_con, "student_id_crosswalk", cross)
+RSQLite::dbWriteTable(ies_research_con, "student_id_crosswalk", cross, overwrite = T)
 
